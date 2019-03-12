3142ac7e8f7edde6b6b16428e3fb6bff6958c372
Unknown <manualsplash@gmail.com>
2019-03-13 00:37:45 +0530

    Add login view changes

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,16 +1,28 @@
 // @flow
 import React, { Component } from 'react';
-import { Typography, TextField } from '@material-ui/core';
+import {
+  Typography,
+  TextField,
+  InputAdornment,
+  IconButton,
+  Button
+} from '@material-ui/core';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+
+const db = require('../actions/dbConnection');
 
 type Props = {};
 
 export default class Home extends Component<Props> {
   props: Props;
+
   constructor(props) {
     super(props);
     this.state = {
-      age: '10',
-      name: 'lakshan'
+      name: '',
+      password: '',
+      showPassword: false
     };
   }
 
@@ -20,18 +32,85 @@ export default class Home extends Component<Props> {
     });
   };
 
+  handleClickShowPassword = () => {
+    this.setState(state => ({ showPassword: !state.showPassword }));
+  };
+
   render() {
+    const users = db.getUsers();
+    console.log('users', users);
+    const { name, showPassword, password } = this.state;
     return (
-      <div>
-        <Typography variant="h3">Welcome back!</Typography>
-        <TextField
-          id="outlined-name"
-          label="Name"
-          value={this.state.name}
-          onChange={this.handleChange('name')}
-          margin="normal"
-          variant="outlined"
-        />
+      <div
+        style={{
+          width: '40%',
+          height: window.innerHeight,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          // backgroundColor: 'cyan',
+          margin: '0 auto'
+        }}
+      >
+        <div style={{ width: '350px' }}>
+          <Typography style={{ paddingBottom: '20px' }} variant="h5">
+            Welcome back!
+          </Typography>
+        </div>
+
+        <div style={{ display: 'block' }}>
+          <TextField
+            id="outlined-name"
+            label="Name"
+            value={name}
+            onChange={this.handleChange('name')}
+            margin="normal"
+            variant="outlined"
+            style={{ width: '350px' }}
+          />
+        </div>
+        <div>
+          <TextField
+            id="outlined-adornment-password"
+            variant="outlined"
+            type={showPassword ? 'text' : 'password'}
+            label="Password"
+            value={password}
+            onChange={this.handleChange('password')}
+            style={{ width: '350px' }}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="Toggle password visibility"
+                    onClick={this.handleClickShowPassword}
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              )
+            }}
+          />
+        </div>
+        <div
+          style={{
+            // display: 'flex',
+            // justifyContent: 'flex-end',
+            width: '350px',
+            marginTop: '20px'
+          }}
+        >
+          <Button
+            style={{
+              backgroundColor: '#3F51B5',
+              float: 'right',
+              color: 'white'
+            }}
+          >
+            Login
+          </Button>
+        </div>
       </div>
     );
   }
