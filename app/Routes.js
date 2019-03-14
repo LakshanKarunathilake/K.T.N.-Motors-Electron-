eeea32095a86464a3c22d361dc1a2b5cae6a0855
Unknown <manualsplash@gmail.com>
2019-03-14 19:55:23 +0530

    Add Home Page
    Include clock dependency and moment dependency

--- app/Routes.js
+++ app/Routes.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Switch, Route } from 'react-router';
 import routes from './constants/routes';
 import App from './containers/App';
-import HomePage from './containers/HomePage';
+import LoginPage from './containers/LoginPage/LoginPage';
+import HomePage from './containers/HomePage/HomePage';
 import CounterPage from './containers/CounterPage';
 
 export default () => (
@@ -10,6 +11,7 @@ export default () => (
     <Switch>
       <Route path={routes.COUNTER} component={CounterPage} />
       <Route path={routes.HOME} component={HomePage} />
+      <Route path={routes.LOGIN} component={LoginPage} />
     </Switch>
   </App>
 );
