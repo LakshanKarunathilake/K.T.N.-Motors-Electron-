6381011270a457e7f1ec4dcd5e6c5cee9dc6fbc0
Unknown <manualsplash@gmail.com>
2019-03-16 23:56:37 +0530

    Add sequelize instance creation in dbConnection

--- app/Databse/dbConnection.js
+++ app/Databse/dbConnection.js
@@ -1,10 +1,20 @@
+import CONFIGS from '../../config/config.json';
+
 const mysql = require('mysql');
 const swal = require('sweetalert');
 // First you need to create a connection to the db
 // Add the credentials to access your database
-const exports = (module.exports = {});
+const Sequelize = require('sequelize');
 
-exports.openConnection = () => {
+// Option 1: Passing parameters separately
+const { database, username, password, dialect } = CONFIGS.development;
+const sequelize = new Sequelize(database, username, password, {
+  host: 'localhost',
+  dialect
+});
+global.sequelize = sequelize;
+
+export const openConnection = () => {
   const con = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -27,7 +37,7 @@ exports.openConnection = () => {
   return con;
 };
 
-exports.closeConnection = con => {
+export const closeConnection = con => {
   con.end(err => {
     // The connection is terminated gracefully
     // Ensures all previously enqueued queries are still
